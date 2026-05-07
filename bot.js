8036e4dc62888e29504d2724c221ca8c1ede74d1
propeeee <mrgamenova585@gmail.com>
2026-05-07 10:37:48 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bot is online!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Web server running on port ${PORT}`);
+});
+
 const fs = require('fs');
 
 function loadData(file, fallback = {}) {
